5b183827631dc1f6c01b22e0a43f042e6d9622d2
andreasiciliano <andreasiciliano@users.noreply.github.com>
2017-11-28 10:23:18 -0800

    Update wf.py

--- weather-forecast/wf.py
+++ weather-forecast/wf.py
@@ -5,7 +5,8 @@ from datetime import datetime, timedelta
 import time
 from collections import namedtuple
 import pandas as pd  
-import requests  
+import requests
+import os
 #import matplotlib.pyplot as plt
 
 API_KEY = ''
@@ -46,35 +47,29 @@ def extract_weather_data(url, api_key, target_date, state, city, days):
         target_date += timedelta(days=1)
     return records
 
-# <<< BEGIN
-# 2017-11-21; I started writing logic to handle multi-day data collection
-# but Giada woke up, so I just kicked off collection of first 500 records
-# and commented out
-# 
-# set filename that stores weather info for the desired location
-# filename = 'weather_records_{}_{}.csv'.format(STATE, CITY)
-#
-# Look for csv file with weather info for the desired location
-#if os.path.isfile(fname):
-    # if file exists, then load into into dataframe, then set target date as one
-    # day past the max target date in the dataframe.
-    # [Note: I'm not explicitly handling the case where file exists but is empty]
-#    df = pd.read_csv(filename)
-#    target_date = datetime(X)
-#else:
+
+filename = 'weather_records_{}_{}.csv'.format(STATE, CITY)
+
+# Look for csv file that stores weather info for the desired location
+if os.path.isfile(fname):
+    # if file exists, then load into into dataframe and set new target date
+    # [Note: not handling the case where file exists but is empty]
+    df = pd.read_csv(filename)
+    target_date = datetime.strptime(df['date'].max(), '%Y-%m-%d')
+                + timedelta(days=1)
+else:
     # if file doesn't exist, then set target date to '2010-01-01' 
     # and create an empty dataframe
-#    df = pd.DataFrame()
-# >>> END
+    df = pd.DataFrame()
+    target_date = datetime(2010, 1, 1)
 
+# Retrieve historical records and save them to a temporary DataFrame
+records = extract_weather_data(BASE_URL, API_KEY, target_date, STATE, CITY, 500)
+tmp_df = pd.DataFrame(records, columns=features)
 
-# First 500 records
-#target_date = datetime(2010, 1, 1)
-#records = extract_weather_data(BASE_URL, API_KEY, target_date, STATE, CITY, 500)
-#df = pd.DataFrame(records, columns=features)
-#df.to_csv('records.csv')
+# Append new records to existing records (to an empty DataFrame if this is the
+# first retrieval
+df = df.append(tmp_df)
 
-# Upload more records
-filename = 'weather_records_{}_{}.csv'.format(STATE, CITY)
-df = pd.read_csv(filename)
-target_date = datetime.strptime(df['date'].max(), '%Y-%m-%d') + timedelta(days=1)
+# Save records to csv file
+df.to_csv(filename)
